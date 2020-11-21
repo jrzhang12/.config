51a74dd43d667b8dc0ce0f2820573c6c37acceeb
Jiarui Zhang <jrzhang9912@gmail.com>
2020-11-20 20:06:53 -0500

    updated zsh, ranger and vimium

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,10 @@
-export XDG_CONFIG_HOME=$HOME/.config
-export LOCALBIN=$XDG_CONFIG_HOME/bin
+# export XDG_CONFIG_HOME=$HOME/.config
+# export LOCALBIN=$XDG_CONFIG_HOME/bin
 export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:$HOME/.npm-global/bin
 export TERM=xterm-256color
+export TERM_ITALICS=true
 export EDITOR=nvim
 export RANGER_LOAD_DEFAULT_RC=false
 export ZIM_HOME="$HOME/.zim"
+export ZSH_AUTOSUGGEST_USE_ASYNC
--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,3 +1,5 @@
+# Mark the name as a function
+# some other useful info: http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
 autoload edit-command-line; zle -N edit-command-line
 bindkey -v
 
@@ -8,8 +10,8 @@ bindkey -M vicmd "j" vi-backward-char
 bindkey -M vicmd "l" vi-forward-char
 bindkey -M vicmd "J" vi-beginning-of-line
 bindkey -M vicmd "L" vi-end-of-line
-bindkey -M vicmd "k" down-line-or-history
 bindkey -M vicmd "i" up-line-or-history
+bindkey -M vicmd "k" down-line-or-history
 bindkey -v '^?' backward-delete-char
 
 # Change cursor shape for different vi modes.
@@ -25,20 +27,19 @@ function zle-keymap-select {
   fi
   zle reset-prompt
 }
+zle -N zle-keymap-select
 
 # Use beam shape cursor on startup
 echo -ne '\e[5 q'
 
 # Use beam shape cursor for each new prompt
 preexec() {
-	echo -ne '\e[5 q'
+  echo -ne '\e[5 q'
 }
 
 _fix_cursor() {
    echo -ne '\e[5 q'
 }
 precmd_functions+=(_fix_cursor) # Use beam shape cursor for each new prompt.
-
-zle -N zle-keymap-select
 
 KEYTIMEOUT=1
